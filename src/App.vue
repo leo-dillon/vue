d4245609f533b9b2c48e90b5ba01d7e525ce9b38
emess9 <ezequiel.messina@davinci.edu.ar>
2024-11-09 13:08:17 -0300

    Resolviendo Merge

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,6 @@ onMounted(async () => {
 <template>
   <Header />
   <HomeView  :productosMostrar="productos" />
-
-  <p>pepito el pistolero</p>
-  <p>pepito el pistolero</p>
-  <p>pepito el pistolero</p>
-  <p>pepito el pistolero</p>
-  <p>pepito el pistolero</p>
-
-  <HomeView  :productosMostrar="productos" />
-
   <Footer />
   <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
