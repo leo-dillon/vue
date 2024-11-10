f0ea8ab84de4d662d27a465be6aaa2ab64412bc5
Malenacaparros <malena.caparros@davinci.edu.ar>
2024-11-10 17:12:27 -0300

    Adición de Seccion Nuestro Equipo dentro del archivo Nosotros.vue

--- src/views/Nosotros.vue
+++ src/views/Nosotros.vue
@@ -17,4 +17,37 @@
             </p>
         </div>
     </section>
+    <section class="container my-4">
+        <h2 class="text-center p-1 pt-2">Nuestro Equipo</h2>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-column flex-md-row justify-content-around align-items-center my-2">
+                <div class="px-1">
+                    <img class="rounded-circle burbuja-1" src="../../public/equipo.png" alt="Leonardo Dillon">
+                    <p class="py-3 text-center"><strong>Leonardo Dillon</strong></p>
+                    <p></p>
+                </div>
+                <div class="px-1">
+                    <img class="rounded-circle burbuja-2" src="../../public/equipo.png" alt="Malena Caparrós">
+                    <p class="py-3 text-center"><strong>Malena Caparrós</strong></p>
+                    <p></p>
+                </div>
+                <div class="px-1">
+                    <img class="rounded-circle burbuja-3" src="../../public/equipo.png" alt="Ezequiel Messina">
+                    <p class="py-3 text-center"><strong>Ezequiel Messina</strong></p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="col px-2">
+                <p class="px-1 text-center">
+                    Cada miembro de nuestro equipo desempeña un papel fundamental para asegurarse de que nuestros clientes reciban un servicio excepcional, desde la selección de los productos más innovadores hasta la buena navegación por nuestra web.
+                </p>
+                <p class="px-1 text-center">
+                    Nos enorgullece trabajar en conjunto, aprovechando nuestras habilidades y conocimientos para mejorar continuamente nuestros procesos. Con un enfoque en la calidad, la innovación y la satisfacción del cliente, nuestro equipo está siempre listo para ayudarte a encontrar lo que necesitas y hacer de tu experiencia de compra algo único.
+                </p>
+            </div>
+        </div>
+    </section>
 </template>
+<style>
+
+</style>
