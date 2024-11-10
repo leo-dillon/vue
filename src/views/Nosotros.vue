9a6f1f882ccb9e71b6e675e561bfaed7ca1540ad
Malenacaparros <malena.caparros@davinci.edu.ar>
2024-11-10 17:45:05 -0300

    Actualización de Seccion Nuestro Equipo dentro del archivo Nosotros.vue

--- src/views/Nosotros.vue
+++ src/views/Nosotros.vue
@@ -3,8 +3,8 @@
         <h1 class="text-center p-1 pt-2">Acerca de Nosotros</h1>
         <div class="d-flex flex-column-reverse flex-md-row-reverse justify-content-around align-items-center">
             <img class="img-fluid" src="../../public/user-tech.png" alt="User tech">
-            <p class="px-1 text-center">
-                En Tech Store, nos apasiona la tecnología y nos dedicamos a ofrecerte lo último en productos electrónicos y gadgets innovadores. Desde smartphones y laptops hasta accesorios y dispositivos inteligentes, contamos con una amplia variedad de artículos de alta calidad para satisfacer tus necesidades tecnológicas. Nos esforzamos por brindarte una experiencia de compra fácil, segura y confiable, con un equipo de expertos siempre dispuesto a ayudarte a encontrar lo que necesitas. ¡Conéctate al futuro con nosotros y descubre cómo la tecnología puede mejorar tu vida!
+            <p class="px-1 text-center lh-sm">
+                En Tech Store, nos apasiona la tecnología y nos dedicamos a ofrecerte lo último en <strong>productos electrónicos</strong>  y gadgets <strong>innovadores</strong>. Desde smartphones y laptops hasta accesorios y dispositivos inteligentes, contamos con una amplia variedad de artículos de alta calidad para satisfacer tus necesidades tecnológicas. Nos esforzamos por brindarte una experiencia de compra <strong>fácil, segura y confiable</strong>, con un equipo de expertos siempre dispuesto a ayudarte a encontrar lo que necesitas. ¡Conéctate al futuro con nosotros y descubre cómo la tecnología puede mejorar tu vida!
             </p>
         </div>
     </section>
@@ -12,9 +12,12 @@
         <h2 class="text-center p-1 pt-2">Nuestra Historia</h2>
         <div class="d-flex flex-column-reverse flex-md-row justify-content-around align-items-center">
             <img class="img-fluid" src="../../public/moto3.png" alt="Motorola 3">
-            <p class="px-1 text-center">
-                Nuestra tienda nació este año como un proyecto 100% virtual, impulsada por el compromiso de ofrecer una experiencia de compra moderna y accesible. Nos especializamos en productos tecnológicos de última generación, seleccionados cuidadosamente para satisfacer las necesidades de nuestros clientes en el mundo digital. A través de nuestra plataforma, buscamos que la tecnología esté al alcance de todos, brindando un servicio confiable y práctico. ¡Bienvenidos a nuestra tienda en línea, donde la innovación está a solo un clic de distancia!
-            </p>
+            <div>
+                <p class="px-1 text-center lh-sm">
+                    Nuestra tienda nació este año como un proyecto 100% virtual, impulsada por el compromiso de ofrecer una experiencia de compra <strong>moderna y accesible</strong>. Nos especializamos en productos tecnológicos de última generación, seleccionados cuidadosamente para satisfacer las necesidades de nuestros clientes en el mundo digital. A través de nuestra plataforma, buscamos que la tecnología esté al alcance de todos, brindando un servicio confiable y práctico.
+                </p>
+                <p class="text-center"><em>¡Bienvenidos a nuestra tienda en línea, donde la innovación está a solo un clic de distancia!</em></p>
+            </div>
         </div>
     </section>
     <section class="container my-4">
@@ -22,32 +25,37 @@
         <div class="d-flex flex-column">
             <div class="d-flex flex-column flex-md-row justify-content-around align-items-center my-2">
                 <div class="px-1">
-                    <img class="rounded-circle burbuja-1" src="../../public/equipo.png" alt="Leonardo Dillon">
-                    <p class="py-3 text-center"><strong>Leonardo Dillon</strong></p>
-                    <p></p>
+                    <img class="rounded-circle burbuja" src="../../public/equipo.png" alt="Leonardo Dillon">
+                    <p class="pt-3 text-center"><strong>Leonardo Dillon</strong></p>
+                    <p><em>info</em></p>
                 </div>
                 <div class="px-1">
-                    <img class="rounded-circle burbuja-2" src="../../public/equipo.png" alt="Malena Caparrós">
-                    <p class="py-3 text-center"><strong>Malena Caparrós</strong></p>
-                    <p></p>
+                    <img class="rounded-circle burbuja" src="../../public/equipo.png" alt="Malena Caparrós">
+                    <p class="pt-3 text-center"><strong>Malena Caparrós</strong></p>
+                    <p><em>info</em></p>
                 </div>
                 <div class="px-1">
-                    <img class="rounded-circle burbuja-3" src="../../public/equipo.png" alt="Ezequiel Messina">
-                    <p class="py-3 text-center"><strong>Ezequiel Messina</strong></p>
-                    <p></p>
+                    <img class="rounded-circle burbuja" src="../../public/equipo.png" alt="Ezequiel Messina">
+                    <p class="pt-3 text-center"><strong>Ezequiel Messina</strong></p>
+                    <p><em>info</em></p>
                 </div>
             </div>
             <div class="col px-2">
-                <p class="px-1 text-center">
-                    Cada miembro de nuestro equipo desempeña un papel fundamental para asegurarse de que nuestros clientes reciban un servicio excepcional, desde la selección de los productos más innovadores hasta la buena navegación por nuestra web.
+                <p class="px-1 text-center lh-sm">
+                    Cada miembro de nuestro equipo desempeña un papel fundamental para asegurarse de que nuestros clientes reciban un <strong>servicio excepcional</strong>, desde la selección de los productos más innovadores hasta la buena navegación por nuestra web.
                 </p>
-                <p class="px-1 text-center">
-                    Nos enorgullece trabajar en conjunto, aprovechando nuestras habilidades y conocimientos para mejorar continuamente nuestros procesos. Con un enfoque en la calidad, la innovación y la satisfacción del cliente, nuestro equipo está siempre listo para ayudarte a encontrar lo que necesitas y hacer de tu experiencia de compra algo único.
+                <p class="px-1 text-center lh-sm">
+                    Nos enorgullece trabajar en conjunto, aprovechando nuestras habilidades y conocimientos para mejorar continuamente nuestros procesos. Con un enfoque en <strong>la calidad, la innovación y la satisfacción</strong> del cliente, nuestro equipo está siempre listo para ayudarte a encontrar lo que necesitas y hacer de tu experiencia de compra algo único.
                 </p>
             </div>
         </div>
     </section>
 </template>
 <style>
-
+    .burbuja{
+        max-width: 200px;
+    }
+    p{
+        font-size: 1.2rem;
+    }
 </style>
