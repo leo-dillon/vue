09f8c76d19fc94a38ca2c6c983e75747e7d382d1
Malenacaparros <malena.caparros@davinci.edu.ar>
2024-11-10 12:25:00 -0300

    Borrar prueba

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,6 @@ export default {
 <template>
   <Header />
     <HomeView  :productosMostrar="productos" />
-    <p>Hola Mundo!</p>
   <Footer />
   <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
