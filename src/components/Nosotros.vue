5f301d6b5c64924729e7d2660338e0ccc0693369
Malenacaparros <malena.caparros@davinci.edu.ar>
2024-11-10 13:12:25 -0300

    Creación de la seccion Nosotros

---
+++ src/components/Nosotros.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="acerca_de_nosotros">
+        <h1>Acerca de Nosotros</h1>
+        <img src="user-tech.png" alt="User tech">
+        <p>En Tech Store, nos apasiona la tecnología y nos dedicamos a ofrecerte lo último en productos electrónicos y gadgets innovadores. Desde smartphones y laptops hasta accesorios y dispositivos inteligentes, contamos con una amplia variedad de artículos de alta calidad para satisfacer tus necesidades tecnológicas. Nos esforzamos por brindarte una experiencia de compra fácil, segura y confiable, con un equipo de expertos siempre dispuesto a ayudarte a encontrar lo que necesitas. ¡Conéctate al futuro con nosotros y descubre cómo la tecnología puede mejorar tu vida!</p>
+    </section>
+    <section class="nuestra_historia">
+        <h2>Nuestra Historia</h2>
+        <img src="public/moto3.png" alt="Motorola 3">
+        <p>Nuestra tienda nació este año como un proyecto 100% virtual, impulsada por el compromiso de ofrecer una experiencia de compra moderna y accesible. Nos especializamos en productos tecnológicos de última generación, seleccionados cuidadosamente para satisfacer las necesidades de nuestros clientes en el mundo digital. A través de nuestra plataforma, buscamos que la tecnología esté al alcance de todos, brindando un servicio confiable y práctico. ¡Bienvenidos a nuestra tienda en línea, donde la innovación está a solo un clic de distancia!</p>
+    </section>
+</template>
+<style>
+    .acerca_de_nosotros{
+        display: flex;
+        justify-content: space-around;
+    }
+
+
+</style>
