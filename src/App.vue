1b113dd3a61310a96cadb4b5b1e0505c88e3e338
Leonardo Dillon <jeannotegui@gmail.com>
2024-11-10 13:25:41 -0300

    feat(router): agregar implementación de vue-router

--- src/App.vue
+++ src/App.vue
@@ -42,22 +42,8 @@ export default {
   </script>
 <template>
   <Header />
-    <HomeView  :productosMostrar="productos" />
+    <RouterView :productosMostrar="productos"/>
   <Footer />
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
-  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
