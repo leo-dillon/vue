889aab3f16e1689cbe9873766d4727c580de5f3e
emess9 <ezequiel.messina@davinci.edu.ar>
2024-11-10 12:11:31 -0300

    Agregado de boton Detalles en card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,16 +13,25 @@ defineProps({
     required: true
   }
 })
+
+// Método de ejemplo para el botón "Detalle del producto"
+const verDetalleProducto = () => {
+  console.log("Detalle del producto: aún sin funcionalidad implementada");
+}
 </script>
+
 <template>
-    <div class="card mb-4 shadow">
-        <div class="card-img-container">
-            <img :src="imagen" class="card-img-top" alt="Imagen del producto">
-        </div>
-        <div class="card-body d-flex flex-column justify-content-center align-items-center">
-            <h5 class="card-title">{{ nombre }}</h5>
-            <p class="card-text">$ {{ precio }}</p>
-            <button class="btn btn-outline-dark" @click="agregarAlCarrito(producto)">Agregar al carrito</button>
-        </div>
+  <div class="card mb-4 shadow">
+    <div class="card-img-container">
+      <img :src="imagen" class="card-img-top" alt="Imagen del producto">
     </div>
+    <div class="card-body d-flex flex-column justify-content-center align-items-center">
+      <h5 class="card-title">{{ nombre }}</h5>
+      <p class="card-text">$ {{ precio }}</p>
+      <!-- Botón Agregar al carrito -->
+      <button class="btn btn-outline-dark mb-2" @click="agregarAlCarrito(producto)">Agregar al carrito</button>
+      <!-- Botón Detalle del producto con evento de clic -->
+      <button class="btn btn-outline-dark" @click="verDetalleProducto">Detalle del producto</button>
+    </div>
+  </div>
 </template>
